8917ae04c18a20647f27d5233450a2c8009f090a
komachi1ne <isabellevasconcelos862@gmail.com>
2024-06-03 12:27:27 -0300

    Comando 7

---
+++ EtecShop/Models/Avaliacao.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EtecShop.Models;
+
+[Table("Avaliacao")]
+public class Avaliacao
+{
+        
+}
